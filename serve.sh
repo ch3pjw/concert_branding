a02f044c2a0ef5f247990b89c8aeeb726a9b1b17
Paul Weaver <paul@ruthorn.co.uk>
2017-08-03 20:56:31 +0100

    Restructure repository

--- serve.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -e
-
-
-pushd `dirname $0` > /dev/null
-here=$PWD
-popd > /dev/null
-
-docker build --tag concert_branding .
-docker run --rm -p 8080:80 -v $here:/var/www/html concert_branding
