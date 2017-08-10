1c214cbef36e3cc55cc6b0df919ae6971d5d6f57
Paul Weaver <paul@ruthorn.co.uk>
2017-08-10 23:22:26 +0100

    Move serve script

---
+++ serve.sh
@@ -0,0 +1,9 @@
+#! /bin/bash -e
+
+
+pushd `dirname $0` > /dev/null
+here=$PWD
+popd > /dev/null
+
+docker build --tag concert_branding ${here}/serve
+docker run --rm -p 8080:80 -v ${here}/build:/var/www/html concert_branding
