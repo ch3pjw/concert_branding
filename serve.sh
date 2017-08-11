0be37543d05d4bad6a5030ea13878a61b770e5c7
Paul Weaver <paul@ruthorn.co.uk>
2017-08-11 09:27:59 +0100

    Serve up static assets as well as built assets

--- serve.sh
+++ serve.sh
@@ -6,4 +6,8 @@ here=$PWD
 popd > /dev/null
 
 docker build --tag concert_branding ${here}/serve
-docker run --rm -p 8080:80 -v ${here}/build:/var/www/html concert_branding
+docker run --rm \
+    -p 8080:80 \
+    -v ${here}/build:/var/www/html \
+    -v ${here}/static:/var/www/html/static \
+    concert_branding
