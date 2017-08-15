1d236b19087c646c945645342a70c8ee0ddc5a8b
Paul Weaver <paul@ruthorn.co.uk>
2017-08-15 22:23:50 +0100

    Redefine favicon shape

--- src/favicon.py
+++ src/favicon.py
@@ -1,42 +1,50 @@
-from math import sin, acos
 from lxml import etree
+from math import sin, acos, sqrt
 
-from svg.ast import Svg, Path, M, V, H, v, a, Z
+from svg.ast import Svg, Path, M, V, H, a, Z, ViewBox, Circle, Rect
 from svg.shapes import clockwise_square, clockwise_circle
 
 
+def segment_height(chord_width, radius):
+    return radius - sqrt(
+        radius ** 2 - (chord_width / 2) ** 2)
+
+
 if __name__ == '__main__':
+    w = 6
+    big_r = 9
+    little_r = 3
+    sh = segment_height(w, big_r)
     s = Svg(
         Path(
             d=(
-                clockwise_square(32) +
+                clockwise_square(64) +
                 (
-                    M(2, 15),
-                    V(17),
-                    H(6),
-                    V(20),
-                    v(-sin(acos(1/10))),
-                    a(5, 5, 0, 1, 0, 2, 0),
-                    V(17),
-                    H(24),
-                    V(20),
-                    v(-sin(acos(1/10))),
-                    a(5, 5, 0, 1, 0, 2, 0),
-                    V(17),
-                    H(30),
-                    V(15),
-                    H(17),
-                    V(12),
-                    v(sin(acos(1/10))),
-                    a(5, 5, 0, 1, 0, -2, 0),
-                    V(15)
+                    M(8, 29),
+                    V(35),
+                    H(15),
+                    V(40),
+                    a(big_r, big_r, 0, 1, 0, 6, 0),
+                    V(35),
+                    H(43),
+                    V(40),
+                    a(big_r, big_r, 0, 1, 0, 6, 0),
+                    V(35),
+                    H(56),
+                    V(29),
+                    H(35),
+                    V(24),
+                    a(big_r, big_r, 0, 1, 0, -6, 0),
+                    V(29),
+                    H(8)
                 ) +
-                clockwise_circle(3, 16, 7) +
-                clockwise_circle(3, 7, 25) +
-                clockwise_circle(3, 25, 25) +
+                clockwise_circle(little_r, 18, 49 - sh) +
+                clockwise_circle(little_r, 46, 49 - sh) +
+                clockwise_circle(little_r, 32, 24 - big_r + sh) +
                 (Z,)
             )
-        )
+        ),
+        viewBox=ViewBox(0, 0, 64, 64)
     )
     tree = etree.ElementTree(s._etree)
     with open('foo.svg', 'wb') as f:
