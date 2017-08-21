70786eec8537bc13bb47e200c37b6eab50ab6d2b
Emma Johnson <emma.t.johnson@live.co.uk>
2017-08-21 21:07:05 +0100

    Add super cool multicoloured avatar generator

---
+++ src/avatar.py
@@ -0,0 +1,36 @@
+import os
+from lxml import etree
+from math import sqrt
+from random import randint
+
+from svg.ast import (
+    Svg, Path, G, Rect, Circle, Style, Text, M, V, H, a, Z, ViewBox, Kern)
+from svg.shapes import square, circle
+
+
+def random_colour():
+    return 'rgb({r}, {g}, {b})'.format(
+        r=randint(0, 255), g=randint(0,255), b=randint(0, 255)
+    )
+
+
+def make_grid(x, y, size, divisions):
+    grid_square_size = size / divisions
+    for h in range(divisions):
+        for v in range(divisions):
+            yield Rect(
+                x=x + (grid_square_size * h),
+                y=y + (grid_square_size * v),
+                width=grid_square_size,
+                height=grid_square_size,
+                fill=random_colour()
+            )
+
+if __name__ == '__main__':
+    s = Svg(
+        *make_grid(30, 44, 100, 5)
+    )
+    tree = etree.ElementTree(s._etree)
+    with open('../build/foo.svg', 'wb') as f:
+        tree.write(
+            f, pretty_print=True, xml_declaration=True, encoding='utf-8')
